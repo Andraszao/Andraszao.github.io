40592e09d2f4609d3b2c399de71166ed66aa5dcd
Andraszao <112449605+Andraszao@users.noreply.github.com>
2024-11-14 22:28:58 +0000

    Deploying to gh-pages from @ Andraszao/Andraszao.github.io@7908d565ec62281d61449d30e7f306655e919a62 🚀

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCj0scdNRgepm4ZZgtPfh1bGytNvjMfpJA',
+  authDomain: 'project-wish-upon.firebaseapp.com',
+  databaseURL: 'https://project-wish-upon-default-rtdb.firebaseio.com',
+  projectId: 'project-wish-upon',
+  storageBucket: 'project-wish-upon.firebasestorage.app',
+  messagingSenderId: '30510570669',
+  appId: '1:30510570669:web:ce1821ce0bf76f2533fa24',
+  measurementId: 'G-B290VJGTRC'
+};
