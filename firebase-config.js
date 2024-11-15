444adad050f94ff41aa2f25146c62c7d1d5b5533
Andraszao <Andraszao@users.noreply.github.com>
2024-11-15 17:41:43 +0000

    deploy: 033ee6859caea843c0daeec9a9f16667be453660

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXE9UyJeSjcFOOoDucZhoaQOvtn1yTHmk',
+  authDomain: 'taskmaster-6bfbd.firebaseapp.com',
+  projectId: 'taskmaster-6bfbd',
+  storageBucket: 'taskmaster-6bfbd.firebasestorage.app',
+  messagingSenderId: '583197012210',
+  appId: '1:583197012210:web:a2db1887d327b4ae4e606',
+  measurementId: 'G-QP2LY3XC9L'
+}; export default firebaseConfig;
